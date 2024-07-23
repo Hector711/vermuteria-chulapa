5cbbff0fb83b4177d020ddadd1f21a7850f261c0
Héctor Guerra <hectorluengo.xx@gmail.com>
2024-07-23 14:57:21 +0200

    feat:

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -6,13 +6,16 @@ import logo from '@/assets/logo.png';
 export default function Layout() {
   return (
     <>
-      <header>
-        <figure className='w-full flex justify-center h-[200px] '>
+      <header className='flex flex-col  items-center '>
+        <figure className='w-[50vw] justify-center h-[200px] '>
           <img src={logo} alt='' className='h-auto scale-150' />
         </figure>
-        <p className='uppercase text-center '>{mainInfo.location}</p>
+        <p className='uppercase text-center text-[1rem]'>{mainInfo.location}</p>
+        <h2 className='text-center text-[3.5rem]'>Nuestra Carta</h2>
       </header>
-      <Outlet />
+      <main className='flex flex-col gap-4 justify-center '>
+        <Outlet />
+      </main>
     </>
   );
 }
--- src/pages/FirstPage.jsx
+++ src/pages/FirstPage.jsx
@@ -2,16 +2,26 @@ import React from 'react';
 import { useNavigate } from 'react-router-dom';
 
 export default function HomePage() {
-  const navigate  = useNavigate();
+  const navigate = useNavigate();
   return (
     <>
-      <main className='flex flex-col gap-4 justify-center'>
-        <h3 className='text-center'>¿Alguna intolerancia?</h3>
-        <div className='flex justify-center gap-4 '>
-          <button onClick={() => {navigate('/filtros')}}>Si</button>
-          <button onClick={() => {navigate('/menu')}}>No</button>
-        </div>
-      </main>
+      <h3 className='text-center '>¿TIENE ALGUNA INTOLERANCIA?</h3>
+      <div className='flex justify-center gap-14 p-5'>
+        <button
+          onClick={() => {
+            navigate('/filtros');
+          }}
+        >
+          <p className='josefin uppercase'>Si</p>
+        </button>
+        <button
+          onClick={() => {
+            navigate('/menu');
+          }}
+        >
+          <p className='josefin uppercase'>No</p>
+        </button>
+      </div>
     </>
   );
 }
