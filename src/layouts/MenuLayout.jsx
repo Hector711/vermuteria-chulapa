5b978e84a7377020f5a40c7f0ea21c3f5a0c7892
Héctor Guerra <hectorluengo.xx@gmail.com>
2024-08-23 15:30:33 +0200

    feat: :wheelchair:

--- src/layouts/MenuLayout.jsx
+++ src/layouts/MenuLayout.jsx
@@ -1,10 +1,12 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, Link } from 'react-router-dom';
 
 export default function MenuLayout() {
   return (
     <>
       <header id='layout' className='animate-blink'>
-        <h2 className='shadoww'>Menu Chulapa</h2>
+        <Link to='/'>
+          <h2 className='shadoww'>Menu Chulapa</h2>
+        </Link>
       </header>
       <main className='animate-blink menu-page'>
         <Outlet />
